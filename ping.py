3247e60572783715cd2dc39586f839c5633f6296
unknown <2638156997@qq.com>
2024-07-04 19:13:25 +0800

    final

--- ping.py
+++ ping.py
@@ -38,12 +38,6 @@ def checksum_six(data):
     s = (s >> 16) + (s & 0xffff)
     s += s >> 16
     return ~s & 0xffff
-# def checksum_six(msg):
-#     s = 0
-#     for i in range(0, len(msg), 2):
-#         w = ord(msg[i]) + (ord(msg[i + 1]) << 8)
-#         s = ((s+w) & 0xffff) + ((s+w) >> 16)
-#     return ~s & 0xffff
 
 def request_ping(data_type, data_code, data_checksum, data_ID, data_Sequence, payload_body,lenth,ipv):
     #  把字节打包成二进制数据
@@ -72,9 +66,6 @@ def chose_ipv6(host,rawsocket,icmp_packet,dst_addr):
         try:
             # udp_server_sock.bind(0)
             host_addr = addr
-            #print("host:",host_addr)
-            #print("dst:",dst_addr)
-            #print("packet:",icmp_packet)
             rawsocket.bind(host_addr)
         except Exception as a:
             #print("ERROR1:",a)
@@ -103,20 +94,10 @@ def raw_socket(dst_addr, icmp_packet,host,n=4):
     # 发送数据到网络
     if(n==6):
         chose_ipv6(host, rawsocket, icmp_packet, dst_addr)
-        #host_addr = socket.getaddrinfo(host, None, socket.AF_INET6)[0][4][0]
-        #addr_info_list = socket.getaddrinfo(host, None, socket.AF_INET6)
-        #print(addr_info_list)
-        #print(host_addr)
-        #host_addr = "2408:8409:2482:5255:9831:b5e4:d7c1:8d20"
-        #host_addr = "2408:8409:2482:5255:9910:1ac:14a3:61cf"
-        #rawsocket.bind((host_addr, 0))
     else:
         host_addr = socket.gethostbyname(host)
-        print(host)
         #rawsocket.bind(("192.168.221.1",0))
-        print(dst_addr)
         sendt=rawsocket.sendto(icmp_packet, (dst_addr, 80))
-        print(sendt)
     # 返回数据
     return send_request_ping_time, rawsocket, dst_addr
 
@@ -138,10 +119,6 @@ def reply_ping(send_request_ping_time, rawsocket, data_Sequence, ipv, timeout=2)
         received_packet, addr = rawsocket.recvfrom(1024)
         print()
 
-        #re=struct.unpack("B",rep)
-        # print(re)
-        #print(type(re))
-        #print(hex(re))
         if(ipv==6):
             # print("here")
             icmpv6Header = received_packet[0:12]
@@ -198,6 +175,18 @@ def TraceRouteTTL(r,dstn,timetolive=128):
     if(ip_src!=dst_addr):
         return 0
 
+def ttl_restrict(dstn,timetolive):
+    dst_addr = socket.gethostbyname(dstn)
+    RandomID = randint(1, 65534)
+    packet = IP(dst=dst_addr, ttl=timetolive, id=RandomID) / ICMP(id=RandomID, seq=RandomID)
+    respon = sr1(packet, timeout=3, verbose=0)
+    if respon != None:
+        ip_src = str(respon[IP].src)
+        if ip_src == dst_addr:
+            return 1;
+    else:
+        return 0
+
 def resolve_ipv6_dns(host):
     try:
         result = dns.resolver.resolve(host, 'AAAA')
@@ -208,38 +197,27 @@ def resolve_ipv6_dns(host):
         return None
 
 def ping_net(net_addr,host_addr,numb):
-    print(net_addr)
     nets = net_addr.split('.')
-    print(nets)
     net_a = ''
     for net in nets:
-        print(net)
         net = int(net)
         net_b = '{:08b}'.format(net)
-        net_a += net_b
-        print(net_a)    #net_a为二进制网络号
+        net_a += net_b  #net_a为二进制网络号
     pos = net_a.rfind('1')
-    print(pos)
     num = 32-int(pos)-1    #有多少位为0
     #s = list(itertools.product(range(2), repeat=num))
     s = list(itertools.product(range(2), repeat=32-int(numb)))
     #net_c = net_a[:-num]   #前几位不变
     net_c = net_a[:-(32-int(numb))]
-    print(net_c)
     count = pow(2,32-int(numb))
     cnt = 0
     for small in s:
         ss1 = ''.join(map(str, small))
         nnet = net_c + ss1     #获取子网ip
-        print(nnet)
         ip1 = nnet[0:8]
-        print("ip1:",ip1)
         ip2 = nnet[8:16]
-        print("ip2:", ip2)
         ip3 = nnet[16:24]
-        print("ip3:", ip3)
         ip4 = nnet[24:32]
-        print("ip4:", ip4)
         i1 = int(ip1, 2)
         i2 = int(ip2,2)
         i3 = int(ip3,2)
@@ -282,10 +260,9 @@ def for_port(dst_ip, port):
     for port in port_list:
         telnet(dst_addr, port)
 
-def ping(dstn,n=4,q=0,ti=0.7,host="",route=0,timetolive=128,ifnet=0,lenth=56,sample='',ipv=4):
+def ping(dstn,n=4,q=0,ti=0.7,host="",route=0,timetolive=128,ifnet=0,lenth=56,sample='',ipv=4,tr=0):
     send, accept, lost = 0, 0, 0
     sumtime, shorttime, longtime, avgtime = 0, 1000, 0, 0
-    print(n)
     # TODO icmp数据包的构建
     if(ipv==4):
         data_type = 8  # ICMP Echo Request
@@ -293,43 +270,55 @@ def ping(dstn,n=4,q=0,ti=0.7,host="",route=0,timetolive=128,ifnet=0,lenth=56,sam
         data_checksum = 0  # "...with value 0 substituted for this field..."
         data_ID = 0  # Identifier
         data_Sequence = 1  # Sequence number
+        if(tr==1):
+            print("正在构建icmp包头")
     else:   #构建ipv6报文
         data_type = 128  # ICMP Echo Request
         data_code = 0  # must be zero
         data_checksum = 0  # "...with value 0 substituted for this field..."
         data_ID = 1  # Identifier
         data_Sequence = 1  # Sequence number
+        if (tr == 1):
+            print("正在构建icmpv6包头")
     if(sample!=''):
         nums=lenth//len(sample)
         databody = sample * nums
         payload_body = b'{databody}'
-    #print(payload_body)
     else:
         payload_body = b'abcdefghijklmnopqrstuvwabcdefghi'  # data
     # 将主机名转ipv4地址格式，返回以ipv4地址格式的字符串，如果主机名称是ipv4地址，则它将保持不变\
     if(ipv==4):
-        #dst_addr = socket.gethostbyname(dstn)
         if(ifnet==1):
             dst_addr=dstn
         else:
             dst_addr = socket.gethostbyname(dstn)
-            print(dst_addr)
+            if (tr == 1):
+                print("正在解析地址")
     else:
         dst_addr=resolve_ipv6_dns(dstn)
+        if (tr == 1):
+            print("正在解析地址")
     print("正在 Ping {0} [{1}] 具有 {2} 字节的数据:".format(dstn, dst_addr, lenth))
-    #reach=TraceRouteTTL(route,dstn,timetolive)
+    if(route==1):
+        TraceRouteTTL(route,dstn,timetolive)
     numm = 0
+    reach = ttl_restrict(dstn,timetolive);
     for i in range(0, n):
-        print(n)
-        #if(reach==0):
-             #print("can't reach, TTL is too small")
-             #return 0
-
+        if(timetolive!=128):
+            if(reach==0):
+                 print("can't reach, TTL is too small")
+                 return 0
         send = i + 1
         # 请求ping数据包的二进制转换
         icmp_packet = request_ping(data_type, data_code, data_checksum, data_ID, data_Sequence + i, payload_body, lenth, ipv)
+        if(tr==1):
+            print("正在构建数据包")
         # 连接套接字,并将数据发送到套接字
         send_request_ping_time, rawsocket, addr = raw_socket(dst_addr, icmp_packet,host,ipv)
+        if (tr == 1):
+            print("正在创建套接字")
+            print("正在将协议发到网上")
+            print("正在接收数据包")
         # 数据包传输时间
         #print("ipv={0}".format(ipv))
         times = reply_ping(send_request_ping_time, rawsocket, data_Sequence + i,ipv)
@@ -337,7 +326,6 @@ def ping(dstn,n=4,q=0,ti=0.7,host="",route=0,timetolive=128,ifnet=0,lenth=56,sam
             if q == 0:
                 print("来自 {0} 的回复: 字节={1} 时间={2}ms".format(addr,lenth,int(times * 1000)))
                 numm=23
-
             accept += 1
             return_time = int(times * 1000)
             sumtime += return_time
@@ -349,7 +337,6 @@ def ping(dstn,n=4,q=0,ti=0.7,host="",route=0,timetolive=128,ifnet=0,lenth=56,sam
         else:
             lost += 1
             print("请求超时。")
-
         if send == n:
             print("{0}的Ping统计信息:".format(dst_addr))
             print(
@@ -372,26 +359,24 @@ if __name__ == "__main__":
     parser.add_argument('-s', type=int, default=56, help='设置想发送的数据包大小')
     parser.add_argument('-p', type=str, default='', help='设置想发送的范本样式')
     parser.add_argument('-ipv', type=int, default=4, help='输入4使用Ipv4，输入6使用Ipv6')
-    parser.add_argument("-test_port", type=str, default='', help="输入想检查端口号的ip地址")
+    parser.add_argument('-tr', type=int, default=0, help='输入1显示具体操作步骤')
+    parser.add_argument("-test_port", type=str, default='', help="输入想检查端口号的ip地址,若有想检查的端口号，用“”括起来，例如“www.baidu.com 10 23 8000”")
 
     args = parser.parse_args()
 
     if(args.net!=''):
         ips = args.net.split('/')
         net = ips[0]
-        print(net)
         numb = ips[1]
-        print(numb)
         ping_net(net,args.I,numb)
     elif(args.test_port!=''):
         netport = args.test_port.split(' ')
         ip_ad = netport[0]
-        print(ip_ad)
         del netport[0]
         for_port(ip_ad, netport)
     else:
         String = args.ip
         ips=String.split('/')
         for ip in ips:
-            ping(ip,args.c,args.q,args.i,args.I,args.r,args.t,0,args.s,args.p,args.ipv)
+            ping(ip,args.c,args.q,args.i,args.I,args.r,args.t,0,args.s,args.p,args.ipv,args.tr)
             print("\n")
